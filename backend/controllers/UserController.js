7d476bb8bf9626db6be9433011bbfaccf22f06ce
GihoKim <kihokkang2@gmail.com>
2024-03-28 16:03:06 +0900

    user update 및 delete api 개발

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -7,7 +7,6 @@ exports.createUser = async (req, res) => {
     
     // ID 중복 확인
     const existingUser = await User.findOne({ id });
-    console.log("existingUser ::", existingUser);
     if (existingUser) {
       return res.status(400).json({ message: '이미 사용 중인 ID입니다. 다른 ID를 선택해주세요.' });
     }
@@ -24,5 +23,70 @@ exports.createUser = async (req, res) => {
   } catch (error) {
     console.error('Error creating user:', error);
     res.status(500).json({ message: 'Server error' });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+      const userId = req.params.id;
+      const user = await User.findOne({ id: userId });
+      if (!user) {
+          return res.status(404).json({ message: '해당 아이디의 사용자를 찾을 수 없습니다.' });
+      }
+      res.status(200).json(user);
+  } catch (error) {
+      console.error('Error fetching user:', error);
+      res.status(500).json({ message: '서버 오류' });
+  }
+};
+
+exports.updateUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { password, name, age, phone } = req.body;
+
+    // 비밀번호 암호화
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // 업데이트할 사용자 정보 추출
+    const userDataToUpdate = {};
+    if (password) userDataToUpdate.password = hashedPassword;
+    if (name) userDataToUpdate.name = name;
+    if (age) userDataToUpdate.age = age;
+    if (phone) userDataToUpdate.phone = phone;
+
+    // 사용자 정보 업데이트
+    const updatedUser = await User.findOneAndUpdate(
+      { id: userId },
+      userDataToUpdate,
+      { new: true } // 업데이트된 문서 반환
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: '해당 아이디의 사용자를 찾을 수 없습니다.' });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).json({ message: '서버 오류' });
+  }
+};
+
+exports.deleteUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    // 데이터베이스에서 해당 ID를 가진 사용자를 삭제합니다.
+    const deletedUser = await User.findOneAndDelete({ id: userId });
+    if (!deletedUser) {
+      // 삭제된 사용자가 없을 경우 404 응답을 반환합니다.
+      return res.status(404).json({ message: '해당 아이디의 사용자를 찾을 수 없습니다.' });
+    }
+    // 삭제된 사용자의 정보를 반환합니다.
+    res.status(200).json({ message: '사용자가 성공적으로 삭제되었습니다.', deletedUser });
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    // 서버 오류가 발생한 경우 500 응답을 반환합니다.
+    res.status(500).json({ message: '서버 오류' });
   }
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -37,6 +37,134 @@ const UserController = require('../controllers/UserController');
  */
 router.post('/create', UserController.createUser);
 
-// 다른 유저 라우트들 추가 가능
+/**
+ * @swagger
+ * /user/{id}:
+ *   get:
+ *     summary: 사용자 정보 가져오기
+ *     description: ID에 해당하는 사용자의 정보를 검색합니다.
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: 사용자 ID
+ *     responses:
+ *       200:
+ *         description: OK. 사용자 정보 반환
+ *       404:
+ *         description: 찾을 수 없음. 해당 ID의 사용자를 찾을 수 없음.
+ *       500:
+ *         description: 내부 서버 오류. 서버에서 오류가 발생했습니다.
+ */
+router.get('/:id', UserController.getUserById);
+
+/**
+ * @swagger
+ * /user/{id}:
+ *   put:
+ *     summary: 사용자 정보 전체 업데이트
+ *     description: 사용자의 정보를 전체적으로 업데이트합니다.
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: 사용자 ID
+ *         schema:
+ *           type: string
+ *       - in: body
+ *         name: body
+ *         required: true
+ *         description: 업데이트할 사용자 정보
+ *         schema:
+ *           type: object
+ *           properties:
+*             password:
+ *               type: string
+ *             name:
+ *               type: string
+ *             age:
+ *               type: string
+ *             phone:
+ *               type: string
+ *     responses:
+ *       '200':
+ *         description: 업데이트된 사용자 정보 반환
+ *       '404':
+ *         description: 해당 아이디의 사용자를 찾을 수 없음
+ *       '500':
+ *         description: 서버 오류
+ */
+router.put('/:id', UserController.updateUserById);
+
+/**
+ * @swagger
+ * /user/{id}:
+ *   patch:
+ *     summary: 사용자 정보 부분 업데이트
+ *     description: 사용자의 정보 일부를 업데이트합니다.
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: 사용자 ID
+ *         schema:
+ *           type: string
+ *       - in: body
+ *         name: body
+ *         required: true
+ *         description: 업데이트할 사용자 정보
+ *         schema:
+ *           type: object
+ *           properties:
+ *             password:
+ *               type: string
+ *             name:
+ *               type: string
+ *             age:
+ *               type: string
+ *             phone:
+ *               type: string
+ *     responses:
+ *       '200':
+ *         description: 업데이트된 사용자 정보 반환
+ *       '404':
+ *         description: 해당 아이디의 사용자를 찾을 수 없음
+ *       '500':
+ *         description: 서버 오류
+ */
+router.patch('/:id', UserController.updateUserById);
+
+/**
+ * @swagger
+ * /user/{id}:
+ *   delete:
+ *     summary: 사용자 정보 삭제하기
+ *     description: ID에 해당하는 사용자의 정보를 삭제합니다.
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: 사용자 ID
+ *     responses:
+ *       200:
+ *         description: OK. 사용자 정보 삭제 성공
+ *       404:
+ *         description: 찾을 수 없음. 해당 ID의 사용자를 찾을 수 없음.
+ *       500:
+ *         description: 내부 서버 오류. 서버에서 오류가 발생했습니다.
+ */
+router.delete('/:id', UserController.deleteUserById);
 
 module.exports = router;
