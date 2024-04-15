3401dde072ecd2917ff08b54ffca7c747ade4365
GihoKim <kihokkang2@gmail.com>
2024-04-15 16:48:02 +0900

    글쓰기 toast-ui-editor 추가

--- frontend/src/views/board/BoardWrite.vue
+++ frontend/src/views/board/BoardWrite.vue
@@ -1,35 +1,56 @@
 <template>
   <div class="container">
     <h3 class="text-center my-4">글쓰기</h3>
+    <p class="text-muted text-start">
+      <span v-if="$store.getters.user">{{ this.$store.getters.user.name }} 님! </br></span>
+      지식의 나눔은 함께 하는 즐거움의 시작입니다. Amigo에서 새로운 사람들을 만나고,</br>
+       다양한 생각과 경험을 공유해보세요. 지식을 공유함으로써 서로가 발전하고 성장하는 순간을 함께하실 수 있습니다.</p>
     <form @submit.prevent="submitForm">
       <div class="mb-3">
-        <label for="title" class="form-label">제목</label>
         <input v-model="title" type="text" class="form-control" id="title" placeholder="제목을 입력하세요" required>
       </div>
       <div class="mb-3">
-        <label for="content" class="form-label">내용</label>
-        <textarea v-model="content" class="form-control" id="content" rows="5" placeholder="내용을 입력하세요" required></textarea>
+        <Editor :initialEditType="'markdown'" :initialValue="content" height="500px" :options="editorOptions" @change="handleChange"/>
       </div>
-      <button type="submit" class="btn btn-primary">글쓰기</button>
+      <div class="d-flex justify-content-between mb-3">
+        <button type="button" class="btn btn-danger" @click="$goBack()">취소</button>
+        <button type="submit" class="btn btn-primary">저장</button>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/vue-editor';
+
 export default {
+  components: {
+    Editor
+  },
   data() {
     return {
       title: '',
-      content: ''
+      content: '', // TOAST UI Editor의 초기 내용을 저장할 데이터
+      editorOptions: {
+        // TO-DO
+        // hideModeSwitch: false
+      }
     };
   },
   methods: {
+    handleChange(content) {
+      // 에디터 내용이 변경될 때 호출되는 메소드
+      this.content = content;
+    },
     submitForm() {
-      // 글쓰기 기능 구현
-      console.log('제목:', this.title);
+      // 글쓰기 버튼 클릭 시 호출되는 메소드
       console.log('내용:', this.content);
       // 글쓰기 로직을 이곳에 추가하세요
     }
   }
 }
 </script>
+
+<style scoped>
+</style>
