d9e65b3a87d1df02c5880397385aec4baeb6c1a4
GihoKim <kihokkang2@gmail.com>
2024-04-15 15:16:21 +0900

    프론트 사이드 게시판 부분 개발

---
+++ frontend/src/views/board/BoardList.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="board-list" class="mt-4">
+        <table class="table">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col" class="text-center" style="width: 5%;">번호</th>
+                    <th scope="col" style="width: 35%;">제목</th>
+                    <th scope="col" style="width: 20%;">작성자</th>
+                    <th scope="col" class="text-center" style="width: 15%;">작성일</th>
+                    <th scope="col" style="width: 10%;">조회수</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(notice, index) in notices" :key="index">
+                    <td class="text-center">{{ notice.number }}</td>
+                    <td>
+                        <a :href="notice.link"><i class="fas fa-external-link-alt"></i> {{ notice.title }}</a>
+                    </td>
+                    <td class="text-center">
+                        <img :src="notice.thumbnail" class="rounded-circle" style="width: 30px; height: 30px;"
+                            alt="User Thumbnail">{{ notice.user }} 
+                    </td>
+                    <td class="text-center"><i class="far fa-calendar-alt"></i> {{ notice.date }}</td>
+                    <td class="text-center"><i class="fas fa-eye"></i> {{ notice.count }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-center">
+                <li class="page-item" v-for="pageNumber in totalPages" :key="pageNumber"
+                    :class="{ 'active': pageNumber === currentPage }">
+                    <a class="page-link" href="#" @click.prevent="$emit('changePage', pageNumber)">{{ pageNumber }}</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        notices: Array,
+        currentPage: Number,
+        totalPages: Number
+    }
+};
+</script>
+
+<style scoped>
+/* 마우스 오버시 배경색이 바뀌는 효과 */
+#board-list tbody tr:hover {
+    background-color: #f5f5f5;
+    /* 변경할 배경색 */
+}
+</style>
---
+++ frontend/src/views/board/BoardWrite.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h3 class="text-center my-4">글쓰기</h3>
+    <form @submit.prevent="submitForm">
+      <div class="mb-3">
+        <label for="title" class="form-label">제목</label>
+        <input v-model="title" type="text" class="form-control" id="title" placeholder="제목을 입력하세요" required>
+      </div>
+      <div class="mb-3">
+        <label for="content" class="form-label">내용</label>
+        <textarea v-model="content" class="form-control" id="content" rows="5" placeholder="내용을 입력하세요" required></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">글쓰기</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      content: ''
+    };
+  },
+  methods: {
+    submitForm() {
+      // 글쓰기 기능 구현
+      console.log('제목:', this.title);
+      console.log('내용:', this.content);
+      // 글쓰기 로직을 이곳에 추가하세요
+    }
+  }
+}
+</script>
